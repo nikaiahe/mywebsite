a01452ca54cf5382e05dde2572629a10c0528c2c
nikaiahe <89421309+nikaiahe@users.noreply.github.com>
2024-01-09 10:46:59 -0500

    Created Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import beachImage from './images/beach.jpeg'
+import NavigationBar from './Navbar'
+import Home from "./pages/Home"
+import Cool from "./pages/Cool"
+import More from "./pages/More"
+import Stuff from "./pages/Stuff"
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-    <h1 id ="Name">
-      nikaia
-    </h1>
-    <p id ="Student">
-      I am a student at columbia
+    <HashRouter>
+      <div>
+        <NavigationBar></NavigationBar>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/Home" element={<Home/>} />
+          <Route path="/Cool" element={<Cool/>} />
+          <Route path="/More" element={<More/>} />
+          <Route path="/Stuff" element={<Stuff/>} />
+        </Routes>
+      </div>
+      
+    </HashRouter>
+    
+    <p>
+      "hello"
     </p>
-    <p id = "Studying">
-      Studying Computer Engineering
-    </p>
-    <img src = {beachImage} alt = "sdfkhdfsh"></img>
+    
     </>
   )
 }
